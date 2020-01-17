2cb2499dc85b2ddc9ffd8a4a5b863e263b9f4e42
Burak Karahan <burak.karahan@mail.ru>
2020-01-17 15:05:15 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -143,8 +143,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'marylonjd@gmail.com'
-EMAIL_HOST_PASSWORD = '10051995z'
+EMAIL_HOST_USER = '@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 mimetypes.add_type("application/javascript", ".js", True)
